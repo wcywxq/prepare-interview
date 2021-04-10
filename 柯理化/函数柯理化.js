771dbde124826ca6111cc9d6a4ba4b98dc96726d
wangxq02 <wangxq02@zenmen.com>
2021-04-11 00:02:10 +0800

    feat: 函数柯理化

--- 柯理化/函数柯理化.js
+++ 柯理化/函数柯理化.js
@@ -1,3 +1,16 @@
-// function curry(fn) {
+// 使函数从一次调用传入多个参数变成多次调用每次传一个参数
+function curry(fn) {
+  let judge = (...args) => {
+    if (args.length === fn.length) return fn(...args);
+    return (...arg) => judge(...args, ...arg);
+  };
+  return judge;
+}
 
-// }
+function add(a, b, c) {
+  return a + b + c;
+}
+
+add(1, 2, 3);
+let addCurry = curry(add);
+console.log(addCurry(1)(2)(3));
